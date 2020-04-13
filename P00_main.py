9843b340ac4b71a597f1241a33160002aeb10c17
snowflake0123 <st0123-kwgzmph-joestar@i.softbank.jp>
2020-04-13 16:27:04 +0900

    コード3-4に更新(p60)

--- P00_main.py
+++ P00_main.py
@@ -1,25 +1,28 @@
 import os
 
 
+SRC_DIR     = 'D00_dataset/training'
 DST_DIR     = 'D01_estimator'
 EST_FILE    = os.path.join(DST_DIR, 'estimator.h5')
 INFO_FILE   = os.path.join(DST_DIR, 'model_info.txt')
 GRAPH_FILE  = os.path.join(DST_DIR, 'model_graph.png')
 HIST_FILE   = os.path.join(DST_DIR, 'history.pdf')
-INPUT_SIZE  = (28, 28)
-FILTERS     = (32, 64)
+INPUT_SIZE  = (160, 160)
+FILTERS     = (64, 128, 256)
 KERNEL_SIZE = (3, 3)
 POOL_SIZE   = (2, 2)
-DENSE_DIMS  = (1024, 128, 10)
+DENSE_DIMS  = [1024, 128]
 LR          = 1e-3
-DATA_SIZE   = 1000
-BATCH_SIZE  = 128
-EPOCHS      = 30
+BATCH_SIZE  = 32
+EPOCHS      = 100
 VALID_RATE  = 0.2
 
+n_class = len(os.listdir(SRC_DIR))
+DENSE_DIMS.append(n_class)
 
 from P01_model_maker import ModelMaker
 maker = ModelMaker (
+    src_dir     = SRC_DIR,
     dst_dir     = DST_DIR,
     est_file    = EST_FILE,
     info_file   = INFO_FILE,
@@ -31,7 +34,6 @@ maker = ModelMaker (
     pool_size   = POOL_SIZE,
     dense_dims  = DENSE_DIMS,
     lr          = LR,
-    data_size   = DATA_SIZE,
     batch_size  = BATCH_SIZE,
     epochs      = EPOCHS,
     valid_rate  = VALID_RATE
