81fcce2f0d1eddaa447455a4b300316e95a8bc62
snowflake0123 <st0123-kwgzmph-joestar@i.softbank.jp>
2020-04-21 21:06:10 +0900

    コード3-14に更新(p94)

--- P01_model_maker.py
+++ P01_model_maker.py
@@ -14,7 +14,7 @@ class ModelMaker:
     def __init__(self, src_dir, dst_dir, est_file,
                  info_file, graph_file, hist_file,
                  input_size, filters, kernel_size, pool_size, dense_dims,
-                 drop_rate, lr, batch_size, reuse_count, epochs, valid_rate):
+                 lr, batch_size, reuse_count, epochs, valid_rate):
         self.src_dir     = src_dir
         self.dst_dir     = dst_dir
         self.est_file    = est_file
@@ -26,7 +26,6 @@ class ModelMaker:
         self.kernel_size = kernel_size
         self.pool_size   = pool_size
         self.dense_dims  = dense_dims
-        self.drop_rate   = drop_rate
         self.lr          = lr
         self.batch_size  = batch_size
         self.reuse_count = reuse_count
@@ -48,16 +47,13 @@ class ModelMaker:
         # 平滑化層の定義
         x = Flatten()(x)
 
-        # ドロップアウト層の定義
-        x = Dropout(self.drop_rate)(x)
-
         # 全結合層の定義
         for dim in self.dense_dims[:-1]:
             x = mutil.add_dense_layer(x, dim)
 
         # 出力層の定義
         x = mutil.add_dense_layer(
-            x, self.dense_dims[-1], activation='softmax')
+            x, self.dense_dims[-1], use_bn=False, activation='softmax')
 
         # モデル全体の入出力を定義
         model = Model(input_x, x)
