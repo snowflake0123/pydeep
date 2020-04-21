3e71c89b5080ce2a65d11502f8c67ea191db2715
snowflake0123 <st0123-kwgzmph-joestar@i.softbank.jp>
2020-04-21 20:53:09 +0900

    コード3-12に更新(p91)

--- P11_model_util.py
+++ P11_model_util.py
@@ -1,18 +1,25 @@
-from keras.layers import Conv2D, Dense, MaxPooling2D
+from keras.layers import Activation, BatchNormalization, Conv2D, Dense, MaxPooling2D
 from keras.utils import plot_model
 
 
 # 畳み込み層とプーリング層を追加する関数
 def add_conv_pool_layers(x, filters, kernel_size, pool_size, activation='relu'):
-    x = Conv2D(filters, kernel_size, padding='same', activation=activation)(x)
-    x = Conv2D(filters, kernel_size, padding='same', activation=activation)(x)
+    x = Conv2D(filters, kernel_size, padding='same', use_bias=False)(x)
+    x = Activation(activation)(x)
+    x = BatchNormalization()(x)
+    x = Conv2D(filters, kernel_size, padding='same', use_bias=False)(x)
+    x = Activation(activation)(x)
+    x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size)(x)
     return x
 
 
 # 全結合層を追加する関数
-def add_dense_layer(x, dim, activation='relu'):
-    x = Dense(dim, activation=activation)(x)
+def add_dense_layer(x, dim, use_bn=True, activation='relu'):
+    x = Dense(dim, use_bias = not use_bn)(x)
+    x = Activation(activation)(x)
+    if use_bn:
+        x = BatchNormalization()(x)
     return x
 
 
