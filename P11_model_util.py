6c14f90972643ad565e88c74b7a00b5fe7facb96
snowflake0123 <st0123-kwgzmph-joestar@i.softbank.jp>
2020-04-27 17:04:19 +0900

    コード3-20に更新(p118)

--- P11_model_util.py
+++ P11_model_util.py
@@ -1,17 +1,5 @@
-from keras.layers import Activation, BatchNormalization, Conv2D, Dense, MaxPooling2D
+from keras.layers import Activation, BatchNormalization, Dense
 from keras.utils import plot_model
-
-
-# 畳み込み層とプーリング層を追加する関数
-def add_conv_pool_layers(x, filters, kernel_size, pool_size, activation='relu'):
-    x = Conv2D(filters, kernel_size, padding='same', use_bias=False)(x)
-    x = Activation(activation)(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(filters, kernel_size, padding='same', use_bias=False)(x)
-    x = Activation(activation)(x)
-    x = BatchNormalization()(x)
-    x = MaxPooling2D(pool_size)(x)
-    return x
 
 
 # 全結合層を追加する関数
